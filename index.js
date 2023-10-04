1c54cfc24ee0390ec6df731c027775909ff9f82d
Paruj Labpattanapaisan <mammothz@Parujs-MacBook-Pro.local>
2023-10-04 17:14:19 +0700

    feat: assignment 2

---
+++ index.js
@@ -0,0 +1,4 @@
+// Ans = 82  => console.log(calculateTotal(testItem, {}));
+// Ans = 77  => console.log(calculateTotal(testItem, { shipping: 0 }));
+// Ans = 24.25  => console.log(calculateTotal(testItem, { discount: 0.75 }));
+// Ans = 89  => console.log(calculateTotal(testItem, { shipping: 12 }));
